b40f01776b8340425c7f48f21a805830c2272b03
T107 <T107@PC265>
2014-12-01 19:45:50 -0600

    proyecto

---
+++ proyecto_final/src/imagencita.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author T107
+ */
+public class imagencita {
+    private String url;
+    private String titulo;
+    private String descripcion;
+
+    public imagencita() {
+    }
+
+    public imagencita(String url, String titulo, String descripcion) {
+        this.url = url;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+}
